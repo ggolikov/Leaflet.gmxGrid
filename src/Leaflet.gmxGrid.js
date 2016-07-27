b80a86c907410b03d43ec10f40705da0c49b290f
ggolikov <gr.golikov@gmail.com>
2016-07-27 15:01:58 +0300

    remove small errors

--- src/Leaflet.gmxGrid.js
+++ src/Leaflet.gmxGrid.js
@@ -70,7 +70,7 @@ L.GmxGrid = L.Polyline.extend({
             defaultXStep = 0, defaultYStep = 0,
             xStep, yStep,
             isOneDegree = this.options.isOneDegree,
-            customGridStep = this.options.customGridStep, // draws grid in kilometers 
+            customGridStep = this.options.customGridStep, // draws grid in kilometers
             centerY = (map.getCenter().lat * Math.PI) / 180;
 
         for (i = 0; i < gridStepsLength; i++) {
@@ -90,8 +90,7 @@ L.GmxGrid = L.Polyline.extend({
             yStep = defaultYStep;
         }
 
-        var zoom = map.getZoom(),
-            latlngArr = [],
+        var latlngArr = [],
             textMarkers = [];
 
         for (i = Math.floor(x1 / xStep), len1 = Math.ceil(x2 / xStep); i < len1; i++) {
